35a405df7709aeb5a26fb2703f64ab1b9331c48f
pedro <pedro@DESKTOP-GT38H7N>
2017-10-16 22:45:10 -0200

    commit dependencias 16/10/2017

--- ProjetoPP/ParadigmasProgramação/src/servlets/LoginServlet.java
+++ ProjetoPP/ParadigmasProgramação/src/servlets/LoginServlet.java
@@ -1,14 +1,15 @@
 package servlets;
 
-import dominio.Usuario;
-import servicos.ServicoLogin;
+import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+
+import dominio.Usuario;
+import servicos.ServicoLogin;
 
 @WebServlet("/logar")
 public class LoginServlet extends HttpServlet {
@@ -16,7 +17,7 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String login = req.getParameter("cpf");
         String senha = req.getParameter("senha");
-        Usuario usuarioLogado = new ServicoLogin().logar(cpf, senha);
+        Usuario usuarioLogado = new ServicoLogin().logar(login, senha);
 
         if (usuarioLogado != null) {
             req.getSession().setAttribute("usuarioLogado", usuarioLogado.getCpf());
